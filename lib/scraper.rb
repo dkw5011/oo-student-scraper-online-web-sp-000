1e599a3be470873d11a30d41a808023f589276a0
Danielle Bergbower <dkw5011@gmail.com>
2020-03-31 18:04:50 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,22 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
+    doc = Nokogiri::HTML(open(index_url))
     
+    students = {}
+  
+    doc.css("div.roster-cards-container").each do |student|
+      students = {
+        :location => student.css("h2.profile-location"),
+        :name => student.css("div.vitals-text-container h1.profile-name").text
+        }
+      
+    end
+    students
   end
 
   def self.scrape_profile_page(profile_url)
